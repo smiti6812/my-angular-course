fc7d5eb7a5c81501917194ef1e50a3c5581d824c
Schmidt Mihaly <smiti6812@gmail.com>
2024-10-29 06:44:49 +0100

    initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { CourseService } from './CourseService';
+import { Course } from './interfaces/course';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-course';
+  @Input() courses!: any[];
+  courseService : CourseService = Inject(CourseService);
+
+  constructor(private _courseService: CourseService){
+    this.courseService = _courseService;
+    this.courses = this.courseService.getCourses();
+  }
+  onCourseSelected(course:Course) {
+    alert(course.description)
+  }
 }
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, NgModule, Output,EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseService } from  '../CourseService';
+import { Course } from '../interfaces/course';
+
+@Component({
+  selector: 'course-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+  @Input() course!: Course;
+  @Input() Courses!: any [];
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
+  onCourseViewed() {
+
+    this.courseEmitter.emit(this.course);
+
+}
+
+}
